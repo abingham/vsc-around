244de0a31ea6c6332d13e7674b9e399eef779f58
Austin Bingham <austin.bingham@gmail.com>
2018-12-04 15:46:45 +0100

    Fiat lux!

---
+++ README.md
@@ -0,0 +1,11 @@
+# Around extension for VS Code
+
+Manage token around regions.
+
+-----------------------------------------------------------------------------------------------------------
+
+## Features
+
+Puts before and after token around selected regions.
+
+Just invoke the `around.around` command and select the token you want to surround your selection with. Some tokens, like parenthesis, will try to be smart by inserting the correct balancing token. Other tokens, like `"`, will just surround the region with two identical tokens.
---
+++ src/extension.ts
@@ -0,0 +1,51 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    const around = vscode.commands.registerTextEditorCommand('extension.around', 
+                                                             (textEditor, edit) => { around(textEditor, edit); });
+
+    context.subscriptions.push(around);
+}
+
+export function deactivate() {
+}
+
+function insertTokens(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, insBefore: string, insAfter: string) {
+
+    const document = textEditor.document;
+    const newSelections: vscode.Selection[] = [];
+
+    textEditor.edit(editBuilder => {
+
+        textEditor.selections.forEach(selection => {
+
+            const adjust = selection.start.line == selection.end.line ? 1 : 0;
+            editBuilder.insert(selection.start, insBefore);
+            editBuilder.insert(selection.end, insAfter);
+            newSelections.push(new vscode.Selection(selection.start.translate(0, 1), selection.end.translate(0, adjust)));
+
+        });
+    }).then(() => {
+
+        textEditor.selections
+        textEditor.selections = newSelections;
+
+    });
+}
+
+function around(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+    vscode.window.showInputBox().then(function(trigger) {
+        if (trigger === undefined) {
+            return;
+        }
+        const pairs = vscode.workspace.getConfiguration('around').pairs;
+        var tokens = [trigger, trigger];
+        if (trigger in pairs) {
+            tokens = pairs[trigger]
+        }
+        insertTokens(textEditor, edit, tokens[0], tokens[1]);
+    });
+}
