d7ed28aae93c7cb7a3b839ec95e25d0062c2cd25
Austin Bingham <austin.bingham@gmail.com>
2020-01-05 19:36:01 +0100

    Simplified and improved implementation.

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(
         vscode.commands.registerTextEditorCommand(
-            'extension.around', 
+            'extension.around',
             (textEditor, edit) => { around(textEditor, edit); }));
 }
 
@@ -15,29 +15,16 @@ export function deactivate() {
 
 function insertTokens(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, insBefore: string, insAfter: string) {
 
-    const document = textEditor.document;
-    const newSelections: vscode.Selection[] = [];
-
     textEditor.edit(editBuilder => {
-
         textEditor.selections.forEach(selection => {
-
-            const adjust = selection.start.line == selection.end.line ? 1 : 0;
-            editBuilder.insert(selection.start, insBefore);
-            editBuilder.insert(selection.end, insAfter);
-            newSelections.push(new vscode.Selection(selection.start.translate(0, 1), selection.end.translate(0, adjust)));
-
-        });
-    }).then(() => {
-
-        textEditor.selections
-        textEditor.selections = newSelections;
-
+            let selected = textEditor.document.getText(selection);
+            editBuilder.replace(selection, insBefore + selected + insAfter);
+        })
     });
 }
 
 function around(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
-    vscode.window.showInputBox().then(function(trigger) {
+    vscode.window.showInputBox().then(function (trigger) {
         if (trigger === undefined) {
             return;
         }
